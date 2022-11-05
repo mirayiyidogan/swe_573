c30a09194995437d7f3c0743362208e63c83591d
Miray Iyidogan <miray.iyidogan@hotmail.com>
2022-11-05 18:45:47 +0300

    pagination has been added homapage

--- gotya/views/homepage.py
+++ gotya/views/homepage.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from multiprocessing import context
+from gotya.models import ContentModel
+from django.core.paginator import Paginator
 
 def homepage(request):
-    context = {
-        "name" : "Miray İyidoğan"
-    }
-    return render(request, 'pages/home.html', context=context) 
+    contents = ContentModel.objects.order_by('-id')
+    page = request.GET.get('page')
+    paginator = Paginator(contents, 2)
+
+    return render(request, 'pages/home.html', context={
+        'contents' : paginator.get_page(page)
+    }) 
+
+    
