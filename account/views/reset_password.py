b28258d8dde12ddf29da647b1edd6f933407a873
Miray Iyidogan <miray.iyidogan@hotmail.com>
2022-11-06 20:50:05 +0300

    reset-password has been implemented

--- account/views/reset_password.py
+++ account/views/reset_password.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth import update_session_auth_hash 
+#from django.contrib import messages
 
 @login_required(login_url='/')
 def reset_password(request):
@@ -10,6 +11,7 @@ def reset_password(request):
         if form.is_valid():
             user= form.save()
             update_session_auth_hash(request, user)
+            #message.success(request, 'Password has changed succesfully.')
             return redirect('homepage')
     else:
         form=PasswordChangeForm(request.user)
