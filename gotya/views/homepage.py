53a91f03eb651c9ff3e45d0ce7d3f34c29463357
Miray Iyidogan <miray.iyidogan@hotmail.com>
2022-11-06 18:15:12 +0300

    seach module has been implemented

--- gotya/views/homepage.py
+++ gotya/views/homepage.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from multiprocessing import context
 from gotya.models import ContentModel
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 def homepage(request):
+    search = request.GET.get('search')
     contents = ContentModel.objects.order_by('-id')
+    if search:
+        contents = contents.filter(
+            Q(header__icontains=search) | Q(text__icontains=search)
+        ).distinct()
     page = request.GET.get('page')
     paginator = Paginator(contents, 2)
 
