5bcd6e1a4732a5720f2f3ab68927d3670ee1bfa5
Miray Iyidogan <miray.iyidogan@hotmail.com>
2022-11-08 16:02:38 +0300

    Login procedure has been implemented

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from account.views import logout, reset_password, profile_update, registration
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('login', auth_views.LoginView.as_view(
+        template_name = 'pages/login.html'
+    ), name='login'),
     path('logout', logout, name='logout'),
     path("reset-password", reset_password, name="reset-password"),
     # reset-password linkine karşılık reset_password view ı çalışarak url ismi de reset-password olacak
