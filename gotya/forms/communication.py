c0ef53c847d3e99cf27290f1cbca7f5515bc8197
Miray Iyidogan <miray.iyidogan@hotmail.com>
2022-11-21 17:58:42 +0300

    comminication form sturcture has been initiliazed

--- gotya/forms/communication.py
+++ gotya/forms/communication.py
@@ -1,6 +1,8 @@
 from django import forms
 
 class CommunicationForm(forms.Form):
-    email = forms.EmailField(max_length=100)
-    name_surname = forms.CharField(max_length=50)
-    message = forms.CharField(widget=forms.Textarea)
+    email = forms.EmailField(label='e-mail',max_length=100)
+    name_surname = forms.CharField(label='name', max_length=150)
+    message = forms.CharField(label="message", widget=forms.Textarea(attrs={
+        'class': 'form-control'
+    }))
