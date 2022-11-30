2279821a43064834b0df22af713273763c40723d
Miray Iyidogan <miray.iyidogan@hotmail.com>
2022-11-30 16:31:23 +0300

    Delete model has been implemented

--- gotya/views/delete_content.py
+++ gotya/views/delete_content.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from gotya.models import ContactModel
+from gotya.models import ContentModel
 from django.shortcuts import get_object_or_404,redirect
 
 @login_required(login_url='/')
 def delete_content(request, slug):
-    get_object_or_404(ContactModel, slug=slug, user=request.user).delete()
+    get_object_or_404(ContentModel, slug= slug, user= request.user).delete()
     return redirect('myspace')
--- gotya/views/myspace.py
+++ gotya/views/myspace.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 def myspace(request):
     contents= request.user.contents.order_by('-id')
     page = request.GET.get('page')
-    paginator = Paginator(contents, 3)
+    paginator = Paginator(contents, 5)
 
     return render(request, 'pages/myspace.html', context={
         'contents' : paginator.get_page(page),
