d4ce1d17354fd3bc13ecaaad6dee13feb4c29607
Miray Iyidogan <miray.iyidogan@hotmail.com>
2022-12-19 22:54:48 +0300

    Sentry is implemented

--- config/settings/base.py
+++ config/settings/base.py
@@ -190,5 +190,15 @@ LOGGING = {
     }
 }
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
+sentry_sdk.init(
+    dsn=env("SENTRY_DSN"),
+    integrations=[
+        DjangoIntegration(),
+    ],
+    traces_sample_rate=1.0,
+    send_default_pii=True
+)
 
