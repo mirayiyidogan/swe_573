206675eab46b50143e686f8debf5a99eeb1c09ca
Miray Iyidogan <miray.iyidogan@hotmail.com>
2022-12-20 23:37:12 +0300

    Bug fix

--- config/settings/base.py
+++ config/settings/base.py
@@ -15,31 +15,18 @@ from pathlib import Path
 from re import template
 import environ
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 env = environ.Env()
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('MY_SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-
-
-# SECURITY WARNING: don't run with debug turned on in production!
-
-
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('MY_SECRET_KEY')
 
 # Application definition
 
